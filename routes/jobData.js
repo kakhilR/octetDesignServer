f1e9f6517855a83dd05bfe59bd9c1f6460f1f9cb
Akhil <akhilkarenae@gmail.com>
2023-08-28 12:44:12 +0530

    added comments

--- routes/jobData.js
+++ routes/jobData.js
@@ -3,10 +3,9 @@ import { addJobData, getJobData, getJobDatastats, jobStats } from '../controller
 
 const routes = express.Router();
 
+// Todo: can make this a s protected routes by adding authorization of admin scope
 routes.post("/add-job",addJobData);
 routes.post("/add-job-stats",jobStats)
-
-
 routes.get('/jobs',getJobData);
 routes.get('/stats',getJobDatastats)
 
